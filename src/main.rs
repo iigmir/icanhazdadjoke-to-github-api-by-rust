353ee4cda5253bb872ad408fc30523ce7ebd1a9b
iigmir <roc120j@gmail.com>
2020-02-17 12:06:23 +0800

    Do routes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
+use rocket::http::RawStr;
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String
+{
+    format!("Hello, {}!", name.as_str())
+}
+
+#[get("/roly?poly&<song>")]
+fn roly(song: &RawStr) -> String
+{   // /roly?song=T-TA&poly
+    format!("I am {}!", song.as_str())
+}
+
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +21,11 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/",
+        routes![
+            index,
+            hello,
+            roly
+        ]
+    ).launch();
 }
